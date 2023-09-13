60bf7a3a20107f424176475d048ba1ffd8a2166c
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-09-13 18:14:32 +0000

    Variable in Js

---
+++ 01_basic_Java_Script/01_variable_basic.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Ritikraja@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "2122121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword, accountCity,accountState])
