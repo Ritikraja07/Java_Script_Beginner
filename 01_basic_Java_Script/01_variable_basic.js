117aa27ba2960c9d64b299cd6e3a346144c17c84
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-09-14 19:03:52 +0000

    dataTypes

--- 01_basic_Java_Script/01_variable_basic.js
+++ 01_basic_Java_Script/01_variable_basic.js
@@ -13,4 +13,7 @@ accountCity = "Bengaluru"
 
 console.log(accountId);
 
+/* Prefer not to use var
+because of issue in block scope and functional scope */
+
 console.table([accountId,accountEmail,accountPassword, accountCity,accountState])
---
+++ 01_basic_Java_Script/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3); //We are using node JS not browser
+
+console.log(3 + 3); //code readibilty should be high
+
+console.log("Ritik");
+
+let name = "Ritik"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint =>
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
