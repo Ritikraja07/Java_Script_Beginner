ce63b30e5a728a4cb71c30318f731acbf75cdb12
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-09-15 19:18:05 +0000

    comi change

--- 01_basic_Java_Script/04_conversionOperation.js
+++ 01_basic_Java_Script/04_conversionOperation.js
@@ -14,7 +14,7 @@ console.log(2%3);
 console.log("**********************************************");
 
 let str1 = "hello"
-let str2 = "ritik"
+let str2 = " ritik"
 
 let str3 = str1 + str2
 console.log(str3);
