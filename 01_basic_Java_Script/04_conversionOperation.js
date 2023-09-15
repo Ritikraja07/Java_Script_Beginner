ab903d4aedcaf0b4784ec241f992faa2f0a54056
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2023-09-15 19:11:08 +0000

    move conversionOperation

---
+++ 01_basic_Java_Script/04_conversionOperation.js
@@ -0,0 +1,26 @@
+// ***************** Operation ******************
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log("**********************************************");
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+console.log("**********************************************");
+
+let str1 = "hello"
+let str2 = "ritik"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("**********************************************");
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
